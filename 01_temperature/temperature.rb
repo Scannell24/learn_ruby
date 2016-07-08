c3c40f1897fad28bb7cce08f572984b4e90ba2a3
Scannell24 <Scannell24@gmail.com>
2016-07-07 21:09:25 -0400

    3 done

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
 #write your code here
+
+def ftoc(fTemp)
+	(fTemp - 32).to_f * 5/9	
+end
+
+def ctof(cTemp)
+	(cTemp.to_f * 9/5) + 32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,30 @@
 #write your code here
+def add(a, b)
+	a+b
+end
+
+def subtract(a, b)
+	a-b
+end
+
+def sum(items)
+	a=0;
+	items.inject(0){|a,x| a + x }
+end
+
+def multiply(items)
+	a=items[0];
+	items.inject(1){|a,x| a * x }
+end
+
+def power(val, power)
+	val ** power
+end
+
+def factorial(n)
+	if n == 0
+    1
+  else
+    n * factorial(n-1)
+  end
+end
